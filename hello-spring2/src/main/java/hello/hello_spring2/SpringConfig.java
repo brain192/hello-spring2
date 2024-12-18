4610586414274237ebdea20b4c56cc27db55306f
KJH <brain192@naver.com>
2024-12-18 21:31:42 +0900

    12/18

--- hello-spring2/src/main/java/hello/hello_spring2/SpringConfig.java
+++ hello-spring2/src/main/java/hello/hello_spring2/SpringConfig.java
@@ -45,12 +45,16 @@ import hello.hello_spring2.repository.*;
 import hello.hello_spring2.service.MemberService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
 @Configuration
 public class SpringConfig {
+
     private final MemberRepository memberRepository;
+
     public SpringConfig(MemberRepository memberRepository) {
         this.memberRepository = memberRepository;
     }
+
     @Bean
     public MemberService memberService() {
         return new MemberService(memberRepository);
