afb05913eb2caf613bf31a44b03531eeb90de1c7
KJH <brain192@naver.com>
2024-12-18 18:42:26 +0900

    12/18

--- hello-spring2/src/main/java/hello/hello_spring2/SpringConfig.java
+++ hello-spring2/src/main/java/hello/hello_spring2/SpringConfig.java
@@ -1,5 +1,6 @@
 package hello.hello_spring2;
 
+/*
 import hello.hello_spring2.repository.JdbcMemberRepository;
 import hello.hello_spring2.repository.JdbcTemplateMemberRepository;
 import hello.hello_spring2.repository.MemberRepository;
@@ -18,11 +19,13 @@ public class SpringConfig {
     public SpringConfig(DataSource dataSource) {
         this.dataSource = dataSource;
     }
+    */
     /*
     DataSource는 데이터베이스 커넥션을 획득할 때 사용하는 객체다. 스프링 부트는 데이터베이스 커넥션 정보를
     바탕으로 DataSource를 생성하고 스프링 빈으로 만들어둔다. 그래서 DI를 받을 수 있다
     */
 
+/*
     @Bean
     public MemberService memberService() {
         return new MemberService(memberRepository());
@@ -35,4 +38,21 @@ public class SpringConfig {
         return new JdbcTemplateMemberRepository(dataSource);
     }
     //bean 생성
+}
+ */
+
+import hello.hello_spring2.repository.*;
+import hello.hello_spring2.service.MemberService;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+@Configuration
+public class SpringConfig {
+    private final MemberRepository memberRepository;
+    public SpringConfig(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
+    @Bean
+    public MemberService memberService() {
+        return new MemberService(memberRepository);
+    }
 }
