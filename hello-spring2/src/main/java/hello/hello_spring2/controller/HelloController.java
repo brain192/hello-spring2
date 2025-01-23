e68138ce85e5afaeed76e75287cd6d2caf985e07
KJH <brain192@naver.com>
2025-01-23 22:23:34 +0900

    1/23 계산기1,2

--- hello-spring2/src/main/java/hello/hello_spring2/controller/HelloController.java
+++ hello-spring2/src/main/java/hello/hello_spring2/controller/HelloController.java
@@ -1,10 +1,22 @@
 package hello.hello_spring2.controller;
 
+import org.hibernate.sql.ast.tree.expression.Summarization;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import hello.hello_spring2.domain.Cals1;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+import java.util.Stack;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import static java.lang.Integer.sum;
 
 /*
 PostMapping / GetMapping
@@ -63,5 +75,100 @@ public class HelloController {
         public void setName(String name) {
             this.name = name;
         }
+    }
+
+    @GetMapping("/cal1")
+    public String cal1() {
+        return "cal1";
+    }
+
+    @GetMapping("/cal1_ok")
+    public String cal1_ok(@RequestParam("cal1") double cal1, @RequestParam("cal2") double cal2,
+                          @RequestParam("see") String see, Model model) {
+
+
+        double cal3;
+
+        String a="test";
+
+
+        if(Objects.equals(see, "a")) {
+            cal3=cal1+cal2;
+            a="no";
+        }else if(Objects.equals(see, "b")){
+            cal3=cal1-cal2;
+        }else if(Objects.equals(see, "c")){
+            cal3=cal1*cal2;
+        }else if(Objects.equals(see, "d")){
+            cal3=cal1/cal2;
+        }else{
+            cal3=0;
+        }
+        System.out.println(cal1);
+        System.out.println(cal2);
+        System.out.println(see);
+        System.out.println(cal3);
+        System.out.println(a);
+
+        model.addAttribute("cal3",cal3);
+
+        return "cal1_ok";
+    }
+
+    @GetMapping("/cal2")
+    public String cal2() {
+        return "cal2";
+    }
+
+    @GetMapping("/cal2_ok")
+    public String cal2_ok(@RequestParam("see") String see, Model model) {
+
+        model.addAttribute("see",see);
+
+        System.out.println(see);
+
+        Stack<String> s = new Stack<>();
+        List<String> list = new ArrayList<>();
+        Pattern p = Pattern.compile("\\d+|\\D");
+        Matcher m = p.matcher(see);
+
+        while (m.find()) {
+            String split = m.group();
+            list.add(split);
+        }
+
+        for (int i = 0; i < list.size(); i++) {
+            if (!s.isEmpty() && s.peek().equals("*")) {
+                // * 연산자 제거
+                s.pop();
+
+                // 계산할 수 가져오기
+                String pop = s.pop();
+
+                // 곱셈 계산 후 다시 스택에 입력
+                s.push(String.valueOf(Integer.parseInt(pop) * Integer.parseInt(list.get(i))));
+            } else {
+                s.push(list.get(i));
+            }
+        }
+
+        int result = Integer.parseInt(s.get(0));
+
+        for (int i = 1; i < s.size(); i += 2) {
+            String op = s.get(i);
+            switch (op) {
+                case "+":
+                    result += Integer.parseInt(s.get(i + 1));
+                    break;
+                case "-":
+                    result -= Integer.parseInt(s.get(i + 1));
+            }
+        }
+
+        System.out.println(result);
+
+        model.addAttribute("result",result);
+
+        return "cal2_ok";
     }
 }
---
+++ hello-spring2/src/main/java/hello/hello_spring2/domain/Cals1.java
@@ -0,0 +1,13 @@
+package hello.hello_spring2.domain;
+
+public class Cals1 {
+    private double cal3;
+
+    public double getCal3() {
+        return cal3;
+    }
+
+    public void setCal3(double cal3) {
+        this.cal3 = cal3;
+    }
+}
