53661c194df7aeb9ec7d3ebeb0b41c888c13a62d
KJH <brain192@naver.com>
2025-03-10 23:02:19 +0900

    게시판 테스트 jdbc 세션로그인

---
+++ hello-spring2/src/main/java/hello/hello_spring2/etc/LoginInterceptor.java
@@ -0,0 +1,18 @@
+package hello.hello_spring2.etc;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+public class LoginInterceptor implements HandlerInterceptor {
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        HttpSession session = request.getSession();
+        if (session.getAttribute("loggedInUser") == null) {
+            response.sendRedirect("/auth/login");
+            return false;
+        }
+        return true;
+    }
+}
