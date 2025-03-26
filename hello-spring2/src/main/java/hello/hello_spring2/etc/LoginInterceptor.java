cdce3000bbb961cc1c84ea00eb989036e0eecd26
KJH <brain192@naver.com>
2025-03-26 23:21:55 +0900

    게시판 테스트 jdbc 세션로그인

--- hello-spring2/src/main/java/hello/hello_spring2/etc/LoginInterceptor.java
+++ hello-spring2/src/main/java/hello/hello_spring2/etc/LoginInterceptor.java
@@ -6,6 +6,7 @@ import jakarta.servlet.http.HttpSession;
 import org.springframework.web.servlet.HandlerInterceptor;
 
 public class LoginInterceptor implements HandlerInterceptor {
+    /*
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
         HttpSession session = request.getSession();
@@ -15,4 +16,6 @@ public class LoginInterceptor implements HandlerInterceptor {
         }
         return true;
     }
+
+     */
 }
--- hello-spring2/src/main/java/hello/hello_spring2/etc/WebConfig.java
+++ hello-spring2/src/main/java/hello/hello_spring2/etc/WebConfig.java
@@ -6,10 +6,13 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
+
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(new LoginInterceptor())
                 .addPathPatterns("/boards/**") // 게시판 관련 페이지에만 적용
-                .excludePathPatterns("/auth/**"); // 로그인/회원가입 페이지는 제외
+                .excludePathPatterns("/auth/**","/"); // 로그인/회원가입 페이지는 제외
     }
+
+
 }
